2f4643600aeeffa3b494f50615aac734e441f037
philostene <gspillette@gmail.com>
2017-09-26 16:52:19 +0200

    etape 3

---
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -0,0 +1,125 @@
+package co.simplon.factorielle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class FactorielleTest {
+
+	public long calculer(long nbr){
+		long res =0;
+		 Factorielle fac = new Factorielle();
+		 res = fac.calculer(nbr);
+		return res;
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void factorielle_0_egal_moins_1() {
+		//given
+		long nbr = 1;  //fact(nbr)
+	//	long res = 1;  //résultat attendu
+		long resCalcule;
+		//when
+		 
+		 resCalcule = calculer(nbr);
+		 
+		//then
+		// assertEquals(res,resCalcule);
+		// assertTrue(res == resCalcule);
+	}
+	
+	@Test
+	public void factorielle_0_egal_1() {
+		//given
+		long nbr = 0;  //fact(nbr)
+		long res = 1;  //résultat attendu
+		long resCalcule;
+		//when
+		 
+		 resCalcule = calculer(nbr);
+		 
+		//then
+		// assertEquals(res,resCalcule);
+		 assertTrue(res == resCalcule);
+	}
+	
+	@Test @Ignore
+	public void factorielle_1_egal_1() {
+		//given
+				long nbr = 1;  //fact(nbr)
+				long res = 1;  //résultat attendu
+				long resCalcule;
+				//when
+				resCalcule = calculer(nbr);
+				//then
+			//	 assertEquals(res,resCalcule);
+				 assertTrue(res == resCalcule);
+				 
+	}
+	
+	@Test
+	public void factorielle_3_egal_6() {
+		//given
+				long nbr = 1;  //fact(nbr)
+				long res = 1;  //résultat attendu
+				long resCalcule;
+				//when
+				resCalcule = calculer(nbr);
+				 
+				//then
+			//	 assertEquals(res,resCalcule);
+				 assertTrue(res == resCalcule);
+	}
+	@Test 
+	public void factorielle_2_pas_egal_2() {
+		//given
+				long nbr = 2;  //fact(nbr)
+				long res = 10;  //résultat attendu
+				long resCalcule;
+				//when
+				resCalcule = calculer(nbr);
+				//then
+			//	 assertEquals(res,resCalcule);
+				fail("la diff est toto " + (resCalcule - res));
+				assertTrue("la diff est de " + (resCalcule - res),res == resCalcule);
+					 
+				
+					 
+				 
+				 
+				 ;
+	}
+	@Test
+	public void factorielle_3_pas_egal_6() {
+		//given
+				long nbr = 3;  //fact(nbr)
+				long res = 7;  //résultat attendu
+				long resCalcule;
+				//when
+				resCalcule = calculer(nbr);
+				 
+				//then
+			//	 assertEquals(res,resCalcule);
+				 assertTrue(res == resCalcule);
+	}
+
+	@Test (timeout=1)
+	public void timoutFactoriel_100_passes() {
+		//given
+				long nbr = 1;  //fact(nbr)
+				long res = 1;  //résultat attendu
+				long resCalcule =0;
+				//when
+				
+				for (long i=0;i<1000000000L;i++){
+					resCalcule = calculer(nbr);
+				};
+				
+				//then
+			//	 assertEquals(res,resCalcule);
+				 assertTrue(res == resCalcule);
+				 
+	}
+}
