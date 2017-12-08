bde145b237eaa08300220616f58deff183785ab7
philostene <gspillette@gmail.com>
2017-12-08 15:31:59 +0100

    titi

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Factorielle 
 {
-	static Logger LOGGER =  (Logger) LoggerFactory.getLogger(Factorielle.class);
+//	static Logger LOGGER =  (Logger) LoggerFactory.getLogger(Factorielle.class);
 	
 	public Factorielle() {
 		super();
@@ -16,13 +16,13 @@ public class Factorielle
 
 	public long calculer(long n) {
 		
-		LOGGER.info("Msg #1");
-		LOGGER.warn("Msg #2");
-		LOGGER.error("Msg #3");
-		LOGGER.debug("Msg #4");
-		LOGGER.trace("MSG TRACE");
-
-		LOGGER.info("calculer() je suis dans la boucle "+ n);
+//		LOGGER.info("Msg #1");
+//		LOGGER.warn("Msg #2");
+//		LOGGER.error("Msg #3");
+//		LOGGER.debug("Msg #4");
+//		LOGGER.trace("MSG TRACE");
+//
+//		LOGGER.info("calculer() je suis dans la boucle "+ n);
 		
 		if (n > 1) {
 
@@ -30,7 +30,7 @@ public class Factorielle
 
 			return res;
 		} else if (n < 0) {
-			LOGGER.debug("n<0 "+ n);
+//			LOGGER.debug("n<0 "+ n);
 			throw new IllegalArgumentException();
 
 		} else if (n == 0L)     			return 1;
---
+++ src/main/java/co/simplon/factorielle/FactorielleLog.java
@@ -0,0 +1,41 @@
+package co.simplon.factorielle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Factorielle
+ */
+public class FactorielleLog 
+{
+	static Logger LOGGER =  (Logger) LoggerFactory.getLogger(FactorielleLog.class);
+	
+	public FactorielleLog() {
+		super();
+	}
+
+	public long calculer(long n) {
+		
+		LOGGER.info("Msg #1");
+		LOGGER.warn("Msg #2");
+		LOGGER.error("Msg #3");
+		LOGGER.debug("Msg #4");
+		LOGGER.trace("MSG TRACE");
+
+		LOGGER.info("calculer() je suis dans la boucle "+ n);
+		
+		if (n > 1) {
+
+			long res = n * calculer(n-1);
+
+			return res;
+		} else if (n < 0) {
+			LOGGER.debug("n<0 "+ n);
+			throw new IllegalArgumentException();
+
+		} else if (n == 0L)     			return 1;
+
+		return n;
+	}
+
+}
